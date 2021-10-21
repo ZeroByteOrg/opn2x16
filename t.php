209d45062e0abb39ff7865242c86665f19a0b888
ZeroByteOrg <thatguy@zerobyte.org>
2021-10-21 00:55:11 -0500

    ZSM2 format working

--- t.php
+++ t.php
@@ -1,19 +1,28 @@
 #!/usr/bin/php -f
 <?php
+$a = array(1,2,3,4,5);
+$b = array(6,7,8,9,10);
 
-$i = 32;
+$a = array_merge($a,$b);
 
-switch ($i) {
-case 0-19:
-	print "less than 20\n";
-	break;
-case 20-29:
-	print "less than 31\n";
-	break;
-case 30-39:
-	print "in the thirties\n";
-	break;
-case 40-49:
-	print "over the hill\n";
-	break;
-}
+print 'array_merge($a,$b):' . "\n";
+var_dump($a);
+
+$a = array(1,2,3,4,5);
+$a = array_slice($a,2);
+
+print "\n";
+print 'array_slice($a,2)' . "\n";
+var_dump($a);
+
+$a = array(1,2,3,4,5);
+$a = array_merge($a,array_slice($b,0,2));
+$b = array_slice($b,2);
+
+print "\n";
+print 'array_merge($a,array_slice($b,0,2))' . "\n";
+print 'array_slice($b,2)' . "\n";
+var_dump($a);
+var_dump($b);
+
+?>
